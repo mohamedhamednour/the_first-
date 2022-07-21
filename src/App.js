fd1750092a539be77fbe76498a9ecfe6e6007c08
mhamed <mohamedhame2ed@gmail.com>
2022-07-21 18:43:22 -0400

    add

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
-import './App.css';
-import React from 'react';
-import Navbar from './compontents/navbar';
-import Home from './compontents/home';
-import Footer from './compontents/footer';
-import Signup from './compontents/sing/signup';
-import Signin from './compontents/sing/sign-in';
-
+import "./App.css";
+import React from "react";
+import Navbar from "./compontents/navbar";
+import Home from "./compontents/home";
+import Footer from "./compontents/footer";
+import Signup from "./compontents/sing/signup";
+import Signin from "./compontents/sing/sign-in";
 
 import { Route, Routes } from "react-router-dom";
 
-
-
-
 function App() {
- 
   return (
     <>
- <Navbar/>
- <Routes>
- <Route path="/" element={<Home />} />
- <Route path="sign_up" element={<Signup />} />
- <Route path="sign_in" element={<Signin />} />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="sign_up" element={<Signup />} />
+        <Route path="sign_in" element={<Signin />} />
+      </Routes>
 
-
- </Routes>
-
-    <Footer />
-      
-  
+      <Footer />
     </>
   );
 }
--- src/compontents/cards.jsx
+++ src/compontents/cards.jsx
@@ -9,10 +9,20 @@ const Card = () => {
       {/* <div class="row "> */}
       <div align='center' className="container_fluid videos">
         <div className="row">
-    
-      <div className="col-12 col-md-6">1</div>
-      <div className="col-12 col-md-6">2</div>
-
+        <ReactPlayer
+        className="react-player col-12 col-md-6"
+        url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+        width="40%"
+        height="250px"
+        controls="true"
+      />
+      <ReactPlayer
+        className="react-player col-12 col-md-6"
+        url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+        width="40%"
+        height="250px"
+        controls="true"
+      />
         
         </div>
       </div>
